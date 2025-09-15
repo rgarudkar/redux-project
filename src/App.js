6e28b659a952c19c01933390776c691b78d34ddc
Ramgopal Garudkar <ramgopal.garudkar2020@vit.ac.in>
2025-09-15 09:33:42 +0530

    Add Bootstrap and React-Bootstrap for styling; enhance Product component layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Product from './Components/Product';
 function App() {
   return (
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,22 +1,42 @@
-import React from 'react'
-import {useEffect,useState} from 'react'
-
-
+import React from "react";
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Product = () => {
-    const [products,setProducts]=useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(()=>{
-    fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(result=>setProducts(result))
-},[])
-  return (
-    <div>
-      Product Component
-      {JSON.stringify(products)}
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((result) => setProducts(result));
+  }, []);
+
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ height: "150px", width: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.price}</Card.Text>
+          <Button variant="primary">ADD TO CART</Button>
+        </Card.Body>
+      </Card>
     </div>
-  )
-}
+  ));
 
-export default Product
+  return (
+    <>
+      <h1 className="text-center">Products</h1>
+      <div className="row">{cards}</div>
+    </>
+  );
+};
+
+export default Product;
